55e834a459dc6c9eecb55d3f96b5af7c2f83da50
90Digital - Kishor <>
2015-10-21 19:28:28 +0530

    Homepage slider fixes to hide from other pages

--- app/Home/HomeSlider.js
+++ app/Home/HomeSlider.js
@@ -1,14 +1,26 @@
-CasinoDirectives.directive('mainSlider', function() {
-    return {
-      restrict: 'E',
-      scope: false,
-      link: function(scope, elem, attr) {
-        if (attr.type === 'text/javascript-lazy') {
-          var code = elem.text();
-          var f = new Function(code);
-          f();
-        }
-      }
-    };
+CasinoDirectives
+    .directive('mainSlider', function() {
+        return {
+          restrict: 'E',
+          scope: false,
+          link: function(scope, elem, attr) {
+            if (attr.type === 'text/javascript-lazy') {
+              var code = elem.text();
+              var f = new Function(code);
+              f();
+            }
+          }
+        };
+    })
+    .controller('Home', ['$scope','$location','$window', function($scope,$location, $window) {
+       $scope.isHome = function(path) {
+	    if ($window.location.pathname === '/') {
+	      return true;
+	    } else {
+	      return false;
+	    }
+	};
+        
+    }
+]);
     
-});
