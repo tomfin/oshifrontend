52bc2a71a59acdd0bc2bcf5ea870d2a5eb7cc181
tomfin <tom.finlayson@gmail.com>
2015-11-12 11:13:39 +0000

    Updated rego. Removed unneeded overwritten files.

--- app/Auth/Registration/FormRegistration/FormRegistrationFilter.js
+++
@@ -1,9 +0,0 @@
-CasinoFilters
-    .filter('registrationOrder', ['$filter', function($filter) {
-        return function(fields, value) {
-            return $filter('orderBy')(fields, function (field) {
-                return field.localData.order;
-            });
-        };
-    }])
-;
--- app/Auth/Registration/SignUpModalRedirectController.js
+++ app/Auth/Registration/SignUpModalRedirectController.js
@@ -1,7 +1,12 @@
 CasinoControllers
-    .controller('SignUpRedirectController', ['$scope', '$state', 'AuthModalService', function ($scope, $state, AuthModalService) {
+    .controller('SignUpRedirectController', ['$scope', '$state', '$rootScope', 'AuthModalService', function ($scope, $state, $rootScope, AuthModalService) {
         $state.transitionTo('home-rego', $state.params).finally(function () {
             AuthModalService.showRegistration();
+        });
+        $rootScope.$watch('data.user', function(){
+            if ($rootScope.data.user.email) {
+                $rootScope.mainModal.close();
+            }
         });
 
     }]);
